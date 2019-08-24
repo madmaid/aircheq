d6292f04a5c40c5ef5787ab632a38ff867d48436
madmaid <madmaid@users.noreply.github.com>
2019-08-24 17:15:53 +0900

    support netradio feature on NHK API

--- aircheq/operators/parsers/nhk_api.py
+++ aircheq/operators/parsers/nhk_api.py
@@ -6,23 +6,40 @@ import dateutil.parser
 from ... import config
 from . import model
 
-NHK_SERVICES = {
+NHK_NETRADIO_TO_SERVICES = {
         # NHKAPI: RADIRU_STATION
-        'r1': 'r1',
-        'r2': 'r2',
-        'r3': 'fm'
+        'n1': 'r1',
+        'n2': 'r2',
+        'n3': 'fm'
 }
 
 
 class APIKeyError(Exception):
     def __init__(self, message):
         self.message = message
+def program_api(date):
+    api = config.NHK_API_URL
+    key = config.NHK_API_KEY
+    area = config.NHK_API_AREA
+    params = {
+            'area': area,
+            'service': "netradio",
+            'date': date.strftime('%Y-%m-%d'),
+            'apikey': key
+    }
+    req = requests.get(api.format_map(params))
+
+    if req.status_code == 401:
+        raise model.APIKeyError("Invalid API Key, Check a paramater: NHK_API_KEY in your config")
+    req.raise_for_status()
+
+    return req.json()
 
 def json_to_program(json_dict):
 
     program = model.Program()
     program.service = "radiru"
-    program.channel = NHK_SERVICES[json_dict['service']['id']]
+    program.channel = NHK_NETRADIO_TO_SERVICES[json_dict['service']['id']]
     program.channel_jp = json_dict['service']["name"] + json_dict['area']['name']
     program.title = json_dict['title']
 
@@ -30,43 +47,21 @@ def json_to_program(json_dict):
     program.info = '\n'.join(i for i in info if i is not None)
 
     program.start = dateutil.parser.parse(json_dict['start_time'])
-    #_start = dateutil.parser.parse(json_dict['start_time'])
-    #program.start = model.naive_to_UTC(_start)
     program.end = dateutil.parser.parse(json_dict['end_time'])
-    #_end = dateutil.parser.parse(json_dict['end_time'])
-    #program.end = model.naive_to_UTC(_end)
     program.duration = program.end - program.start
 
     program.is_movie = False
     program.is_repeat = False
     return program
 
-def get_programs(key=config.NHK_API_KEY,
-                channels=NHK_SERVICES.keys(),
-                area=config.NHK_API_AREA,
-                api=config.NHK_API_URL):
 
-    if not key:
-        raise APIKeyError("API Key is not found. Check user config")
+def get_programs():
+    today = datetime.date.today()
+    for dayafter in range(7):
+        date = today + datetime.timedelta(days=dayafter)
+        _json = program_api(date)
 
-    for channel in channels:
-        today = datetime.date.today()
-        for d in range(7):
-            date = today + datetime.timedelta(days=d)
-            params = {
-                    'area': area,
-                    'service': channel,
-                    'date': date.strftime('%Y-%m-%d'),
-                    'apikey': key
-            }
-            req = requests.get(api.format_map(params))
+        for ch in _json["list"].values():
+            for program in ch:
+                yield json_to_program(program)
 
-            _json = req.json()
-            try:
-                if _json['fault']['datail']['errorcode'] == "steps.oauth.v2.FailedToResolveAPIKey":
-                    raise APIKeyError("Invalid API Key, Check user config")
-            except KeyError:
-                pass
-
-            for p in _json['list'][channel]:
-                yield json_to_program(p)
