ca9579e08071cb80bfef796ed1c973e55438e3d7
madmaid <madmaid@users.noreply.github.com>
2019-09-18 09:40:48 +0900

    fix default style to work

--- aircheq/web/aircheq_wui/src/components/ModalUtil.tsx
+++ aircheq/web/aircheq_wui/src/components/ModalUtil.tsx
@@ -2,13 +2,26 @@ import React from "react";
 import styled from "styled-components";
 import ReactModal from "react-modal";
 
-const Modal: React.SFC<any> = (props: any) => {
-  const style = {
-    overlay: {
-      backgroundColor: "rgba(0, 0, 0, 0.75)"
-    }
+const Modal: React.SFC<ReactModal.Props> = props => {
+  const defaultBGColor = "rgba(0, 0, 0, 0.75)";
+  const defaultOverlay = {
+    backgroundColor: defaultBGColor
   };
-  return <ReactModal {...props} style={style} />;
+  const defaultStyle = {
+    overlay: defaultOverlay
+  };
+
+  // override transparency
+  let _style = props.style;
+  if (_style == undefined) {
+    _style = defaultStyle;
+  } else if (_style.overlay == undefined) {
+    _style.overlay = defaultOverlay;
+  } else if (_style.overlay.backgroundColor == undefined) {
+    _style.overlay.backgroundColor = defaultBGColor;
+  }
+
+  return <ReactModal {...props} style={_style} />;
 };
 const OpenButton = styled.button``;
 const CloseButton = styled.button`
