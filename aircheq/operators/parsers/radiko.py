e7cdad10162f07620cf2dd922c972423ce5c8599
madmaid <madmaid@users.noreply.github.com>
2021-07-25 19:46:19 +0900

    support new api

--- aircheq/operators/parsers/radiko.py
+++ aircheq/operators/parsers/radiko.py
@@ -29,7 +29,7 @@ def get_channels(radiko_auth=None):
     return dict {"channel": "channel_jp"}
     """
     if radiko_auth is None:
-        radiko_auth = auth.RadikoRTMPAuth()
+        radiko_auth = auth.RadikoHLSAuth()
 
     area_id = radiko_auth.get_area()
     #TODO: fetch all channels for premium
